750e6a844bdfce8c789442bfb0b56caadd8a7d9e
Lyric <lyric@pc.dev>
2025-03-18 14:11:32 -0700

    fix: initial annotation causes page unscrollable

--- src/lib/components/AnnotationBlock.svelte
+++ src/lib/components/AnnotationBlock.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
-  import * as Collapsible from "$lib/components/ui/collapsible";
   import { Button } from "$lib/components/ui/button";
   import * as Dialog from "$lib/components/ui/dialog";
   import { StickyNote } from "@lucide/svelte";
   import { ScrollArea } from "$lib/components/ui/scroll-area";
+  import { pushState } from "$app/navigation";
+  import { tick, onMount } from "svelte";
 
   let { children, referenceMessage, for: messageId } = $props();
 
-  let open = $state(getQueryParameter() === messageId);
+  // NOTE: avoid setting the initial open state to true.
+  // When open state is initially true, the dialog will be open at the start
+  // but closing that dialog will cause the main body to be unscrollable.
+  let open = $state(false);
 
-  $effect(() => {
-    if (open) {
-      setQueryParameter(messageId);
-    } else {
-      deleteQueryParameter();
+  // setting the state after mount to circumvent the above issue
+  onMount(() => {
+    if (getQueryParameter() === messageId) {
+      open = true;
     }
+  });
+
+  // sync the query parameter with the dialog state
+  $effect(() => {
+    void open; // dependency
+    tick().then(() => {
+      if (open) {
+        setQueryParameter(messageId);
+      } else {
+        deleteQueryParameter();
+      }
+    });
   });
 
   function setQueryParameter(messageId: string) {
@@ -22,13 +37,13 @@
     const url = new URL(window.location.href);
     url.searchParams.set("annotation", messageId);
     url.hash = "";
-    window.history.pushState({}, "", url);
+    pushState(url, {});
   }
   function deleteQueryParameter() {
     if (typeof window === "undefined") return;
     const url = new URL(window.location.href);
     url.searchParams.delete("annotation");
-    window.history.pushState({}, "", url);
+    pushState(url, {});
   }
   function getQueryParameter() {
     if (typeof window === "undefined") return;
