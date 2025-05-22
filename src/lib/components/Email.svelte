64e1e9b55fa2947248324c43c5b5550a01146053
Lyric <lyric@pc.dev>
2025-05-21 18:00:30 -0700

    enhance email

--- src/lib/components/Email.svelte
+++ src/lib/components/Email.svelte
@@ -9,9 +9,9 @@
   } from "eml-parse-js";
   import { page } from "$app/state";
   import KvTable from "$lib/components/KvTable.svelte";
-  import * as Collapsible from "$lib/components/ui/collapsible";
-  import Button from "$lib/components/ui/button/button.svelte";
   import { iter } from "$lib/util/iter";
+  import * as Tooltip from "$lib/components/ui/tooltip";
+  import { ChevronDown } from "@lucide/svelte";
 
   const { eml } = $props();
 
@@ -23,18 +23,29 @@
     (async () => {
       const response = await fetch(emailUrl);
       const blob = await response.text();
-      email = readEml(blob);
+      const result = readEml(blob);
+      if (typeof result === "string") {
+        console.error("Error parsing EML:", result);
+        return;
+      }
+      if (result instanceof Error) {
+        console.error("Error parsing EML:", result);
+        return;
+      }
+      email = result;
       console.log(email);
     })();
   });
+
+  let open = $state(true);
 </script>
 
 <div
   class="email flex flex-col gap-1 w-full bg-gray-800 rounded-lg overflow-hidden"
 >
   {#if email}
-    <div class="flex flex-col w-full h-fit p-4">
-      <span class="shrink-0 font-bold">{email.headers.Subject}</span>
+    <div class="flex flex-col w-full h-fit p-4 items-start">
+      <span class="shrink-0 font-bold">{email.subject}</span>
       <span class="text-sm text-gray-400">
         {email.date
           ? new Date(email.date).toLocaleString("en-US", {
@@ -44,18 +55,52 @@
             })
           : ""}
       </span>
-      <span class="text-sm">
-        {iter(email.from).map((o) => o.email)}
-        ->
-        {iter(email.to).map((o) => o.email)}
+      <span class="text-sm flex flex-row gap-2">
+        {#each iter(email.from) as from}
+          <Tooltip.Root openDelay={100}>
+            <Tooltip.Trigger class="hover:bg-gray-200/20">
+              {from.name}
+            </Tooltip.Trigger>
+            <Tooltip.Content
+              class="bg-popover text-popover-foreground softborder"
+            >
+              <p>{from.email}</p>
+            </Tooltip.Content>
+          </Tooltip.Root>
+        {/each}
+        <span> -> </span>
+        {#each iter(email.to) as from}
+          <Tooltip.Root openDelay={100}>
+            <Tooltip.Trigger class="hover:bg-gray-200/20">
+              {from.name}
+            </Tooltip.Trigger>
+            <Tooltip.Content
+              class="bg-popover text-popover-foreground softborder"
+            >
+              <p>{from.email}</p>
+            </Tooltip.Content>
+          </Tooltip.Root>
+        {/each}
       </span>
-      <a href={emailUrl} class="text-sm"> Download as .eml </a>
+      <span class="text-sm flex flex-row justify-between w-full">
+        <span></span>
+        <button
+          onclick={() => (open = !open)}
+          class="transition"
+          class:rotate-360={!open}
+          class:rotate-180={open}
+        >
+          <ChevronDown />
+        </button>
+        <a href={emailUrl}>Download as .eml</a>
+      </span>
     </div>
     {#if email.html}
       <iframe
-        class="w-full h-96 bg-gray-200"
+        class="collapsible w-full h-96 bg-gray-200"
         title={email.headers.Subject}
         srcdoc={email.html}
+        class:open
       >
       </iframe>
     {/if}
@@ -67,5 +112,13 @@
 <style>
   .email {
     box-shadow: 0 0 2px 0 rgba(255, 255, 255, 1);
+  }
+
+  .collapsible {
+    transition: all 0.3s var(--ease-out-expo);
+    overflow: hidden;
+  }
+  .collapsible:not(.open) {
+    height: 0;
   }
 </style>
