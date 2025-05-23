ddfb3a06d228ce24fba507308a485a9fc4d21464
Lyric <lyric@pc.dev>
2025-05-22 23:14:25 -0700

    add tweet component

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -1,12 +1,21 @@
 <script>
-  import { TriangleAlert, OctagonAlert } from "@lucide/svelte";
-  const { color, children, icon } = $props();
+  import { TriangleAlert, OctagonAlert, Info } from "@lucide/svelte";
+  const { color, children, icon = "info" } = $props();
 
-  const Icon = { warning: TriangleAlert, stop: OctagonAlert }[icon];
+  const _colorsForTailwind = {
+    yellow:
+      "text-yellow-400 bg-yellow-600/25 border-yellow-600/50 text-yellow-100/90",
+    blue: "text-blue-400 bg-blue-600/25 border-blue-600/50 text-blue-100/90",
+    sky: "text-sky-400 bg-sky-600/25 border-sky-600/50 text-sky-100/90",
+    cyan: "text-cyan-400 bg-cyan-600/25 border-cyan-600/50 text-cyan-100/90",
+    red: "text-red-400 bg-red-600/25 border-red-600/50 text-red-100/90",
+  };
+
+  const Icon = { warning: TriangleAlert, stop: OctagonAlert, info: Info }[icon];
 </script>
 
 <div
-  class="bg-{color}-600/25 border-1 p-4 font-medium border-{color}-600/50 text-{color}-100/90
+  class="bg-{color}-600/25 border-{color}-600/50 text-{color}-100/90 border-1 p-4 font-medium
  rounded-xl text-sm flex items-start gap-2"
 >
   <Icon size="20" class="text-{color}-400  shrink-0" />
---
+++ src/lib/components/Tweet.svelte
@@ -0,0 +1,61 @@
+<script>
+  import tweets from "$lib/tweets.json";
+  import {
+    MessageSquare,
+    Repeat2,
+    Heart,
+    Bookmark,
+    ExternalLink,
+  } from "@lucide/svelte";
+  import { Card } from "$lib/components/ui/card";
+  import { Button } from "$lib/components/ui/button";
+  const { twid, bold = ["Except he doesn"] } = $props();
+  const tweet = tweets[twid];
+  let tweetHtml = tweet["Tweet Text"];
+  bold.forEach((word) => {
+    tweetHtml = tweetHtml.replace(
+      new RegExp(`(${word})`, "g"),
+      `<mark>$1</mark>`
+    );
+  });
+</script>
+
+<Card class="p-4 flex flex-col gap-2">
+  <div class="flex flex-col">
+    <span class="font-bold">{tweet["Author Name"]}</span>
+    <span class="text-sm text-muted-foreground">
+      <a
+        href="https://twitter.com/{tweet['Author Username']}"
+        rel="external"
+        target="_blank"
+      >
+        @{tweet["Author Username"]}
+      </a>
+    </span>
+  </div>
+  <div>
+    {@html tweetHtml}
+  </div>
+  <div>
+    <span class="text-sm text-muted-foreground">{tweet["Creation Time"]}</span>
+  </div>
+  <div class="flex justify-between text-muted-foreground max-w-100">
+    <div class="flex gap-1 items-center text-sm">
+      <MessageSquare size="20" /><span>{tweet["Reply Count"]}</span>
+    </div>
+    <div class="flex gap-1 items-center text-sm">
+      <Repeat2 size="22" /><span>{tweet["Retweet Count"]}</span>
+    </div>
+    <div class="flex gap-1 items-center text-sm">
+      <Heart size="20" /><span>{tweet["Like Count"]}</span>
+    </div>
+    <div class="flex gap-1 items-center text-sm">
+      <Bookmark size="20" /><span>{tweet["Bookmark Count"]}</span>
+    </div>
+    <div class="flex gap-1 items-center text-sm">
+      <Button variant="ghost" href={tweet["Tweet URL"]} target="_blank">
+        <ExternalLink size="20" />
+      </Button>
+    </div>
+  </div>
+</Card>
