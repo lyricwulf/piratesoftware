608288ca91af0e66a4c754408cf0e86587e1e4bc
Lyric <lyric@pc.dev>
2025-05-22 22:05:41 -0700

    create callout component

---
+++ src/lib/components/Callout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { TriangleAlert, OctagonAlert } from "@lucide/svelte";
+  const { color, children, icon } = $props();
+
+  const Icon = { warning: TriangleAlert, stop: OctagonAlert }[icon];
+</script>
+
+<div
+  class="bg-{color}-600/25 border-1 p-4 font-medium border-{color}-600/50 text-{color}-100/90
+ rounded-xl text-sm flex items-start gap-2"
+>
+  <Icon size="20" class="text-{color}-400  shrink-0" />
+  <div>
+    {@render children()}
+  </div>
+</div>
--- src/routes/WipNotice.svelte
+++ src/routes/WipNotice.svelte
@@ -1,11 +1,7 @@
 <script>
-  import { TriangleAlert } from "@lucide/svelte";
+  import Callout from "$lib/components/Callout.svelte";
 </script>
 
-<div
-  class="bg-yellow-600/25 border-1 p-4 font-medium border-yellow-600/50 text-yellow-100/90
-rounded-xl text-sm flex items-start gap-2"
->
-  <TriangleAlert size="22" class="text-yellow-400" />
+<Callout color="yellow" icon="warning">
   This page is a work in progress. It may be incomplete or contain errors.
-</div>
+</Callout>
