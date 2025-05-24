4cd216385d8cdaa2e00db3d3efa18e3a5b59cb17
Lyric <lyric@pc.dev>
2025-05-23 20:49:10 -0700

    address warnings

--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -46,55 +46,6 @@
   .popup-container {
     max-width: 300px;
   }
-  .popup-container input {
-    display: none;
-  }
-
-  .popup-small {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-
-    border-radius: 8px;
-    overflow: hidden;
-  }
-
-  .popup-small:has(.caption) {
-    margin-block: 1px; /* or the box shadow may cut off */
-    box-shadow: 0 0 2px 0px white;
-  }
-
-  .popup-small .caption {
-    background: hsl(var(--accent));
-    padding: 5px;
-    text-align: center;
-
-    font-size: 0.8rem;
-    color: var(--color-tooltip);
-    word-break: break-word;
-  }
-
-  .popup-large {
-    display: none;
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: 10;
-    justify-content: center;
-    align-items: center;
-  }
-  .popup-large img {
-    max-width: 100vw;
-    max-height: 100vh;
-    animation: pop-up 0.3s var(--ease-out-expo);
-  }
-
-  .popup-container:has(input:checked) .popup-large {
-    display: grid;
-  }
 
   @keyframes pop-up {
     0% {
