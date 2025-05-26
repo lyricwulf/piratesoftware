327eb6829f714f625a225fc2d80145b09b71cb04
Lyric <lyric@pc.dev>
2025-05-25 17:53:23 -0700

    perf: glob specificity

--- src/routes/md-metadata.ts
+++ src/routes/md-metadata.ts
@@ -1,16 +1,12 @@
 import DIR_METADATA from "./dir-metadata.json";
 
-const svxFiles = import.meta.glob("/src/routes/**/*.svx", { eager: true });
+const svxFiles = import.meta.glob("/src/routes/**/+page.svx", { eager: true });
 
 export const NAV_ITEMS = { name: "", children: [], href: "" };
 
 export const MD_METADATA = new Map<string, any>();
 
 for (const [filePath, fileModule] of Object.entries(svxFiles)) {
-  // skip annotations
-  if (filePath.match(/\/annotation-\d*\.svx$/)) {
-    continue;
-  }
   const parts = filePath.split("/");
   const name = parts[parts.length - 2];
   const pagePath = filePath
