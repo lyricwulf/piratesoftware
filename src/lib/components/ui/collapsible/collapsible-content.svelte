5f4f180ecdcaa3e026252f5e5b9960d3f54a8c74
Lyric <lyric@pc.dev>
2025-06-03 16:47:38 -0700

    use ease-out-expo for collapsible

--- src/lib/components/ui/collapsible/collapsible-content.svelte
+++ src/lib/components/ui/collapsible/collapsible-content.svelte
@@ -3,9 +3,14 @@
 	import { slide } from "svelte/transition";
 	type $$Props = CollapsiblePrimitive.ContentProps;
 
+	function easeOutExpo(t: number): number {
+		return t === 1 ? 1 : 1 - Math.pow(2, -10 * t);
+	}
+
 	export let transition: $$Props["transition"] = slide;
 	export let transitionConfig: $$Props["transitionConfig"] = {
-		duration: 150,
+		duration: 300,
+		easing: easeOutExpo,
 	};
 </script>
 
