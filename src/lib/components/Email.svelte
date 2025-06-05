5a06058c1788f2dea64dbd44167cb23011448001
Lyric <lyric@pc.dev>
2025-06-04 22:54:40 -0700

    email loading state use subject

--- src/lib/components/Email.svelte
+++ src/lib/components/Email.svelte
@@ -20,7 +20,7 @@
   const emailUrl = `${pathname}/${eml}`;
 
   let email = $state<ReadedEmlJson>({
-    subject: "Loading...",
+    subject: eml,
     date: "",
     from: [],
     to: [],
@@ -50,107 +50,103 @@
 </script>
 
 <div class="email flex flex-col rounded-lg overflow-hidden softborder">
-  {#if email}
-    <div class="email-banner bg-gray-900 flex flex-row items-center p-4 gap-2">
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 100 100"
-        class="absolute left-0 top-0 w-full h-full opacity-10 pointer-events-none"
-        preserveAspectRatio="none"
-      >
-        <polygon
-          points="0,100 100,100 50,20"
-          stroke="grey"
-          fill="none"
-          stroke-width="1"
-          stroke-linejoin="round"
-        />
-        <polygon
-          points="0,0 100,0 50,80"
-          stroke="white"
-          fill="none"
-          stroke-width="1"
-          stroke-linejoin="round"
-        />
-      </svg>
-      <div class="flex flex-col w-full h-fit items-start z-1">
-        <span class="shrink-0 font-bold bg-gray-900">{email.subject}</span>
-        <span class="text-sm text-gray-400 bg-gray-900">
-          {email.date
-            ? new Date(email.date).toLocaleString("en-US", {
-                dateStyle: "long",
-                timeStyle: "short",
-                timeZone: "PST",
-              })
-            : ""}
-        </span>
-        <span class="text-sm flex flex-row gap-2 bg-gray-900">
-          {#each iter(email.from) as from}
-            <Tooltip.Root openDelay={100}>
-              <Tooltip.Trigger class="hover:bg-gray-200/20">
-                {from.name || from.email}
-              </Tooltip.Trigger>
-              <Tooltip.Content
-                class="bg-popover text-popover-foreground softborder"
-              >
-                <p>{from.email}</p>
-              </Tooltip.Content>
-            </Tooltip.Root>
-          {/each}
-          <span> -> </span>
-          {#each iter(email.to) as to}
-            <Tooltip.Root openDelay={100}>
-              <Tooltip.Trigger class="hover:bg-gray-200/20">
-                {to.name || to.email}
-              </Tooltip.Trigger>
-              <Tooltip.Content
-                class="bg-popover text-popover-foreground softborder"
-              >
-                <p>{to.email}</p>
-              </Tooltip.Content>
-            </Tooltip.Root>
-          {/each}
-        </span>
-      </div>
-      <div>
-        <span class="text-sm flex flex-col">
+  <div class="email-banner bg-gray-900 flex flex-row items-center p-4 gap-2">
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 100 100"
+      class="absolute left-0 top-0 w-full h-full opacity-10 pointer-events-none"
+      preserveAspectRatio="none"
+    >
+      <polygon
+        points="0,100 100,100 50,20"
+        stroke="grey"
+        fill="none"
+        stroke-width="1"
+        stroke-linejoin="round"
+      />
+      <polygon
+        points="0,0 100,0 50,80"
+        stroke="white"
+        fill="none"
+        stroke-width="1"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <div class="flex flex-col w-full h-fit items-start z-1">
+      <span class="shrink-0 font-bold bg-gray-900">{email.subject}</span>
+      <span class="text-sm text-gray-400 bg-gray-900">
+        {email.date
+          ? new Date(email.date).toLocaleString("en-US", {
+              dateStyle: "long",
+              timeStyle: "short",
+              timeZone: "PST",
+            })
+          : ""}
+      </span>
+      <span class="text-sm flex flex-row gap-2 bg-gray-900">
+        {#each iter(email.from) as from}
           <Tooltip.Root openDelay={100}>
-            <Tooltip.Trigger>
-              <Button href={emailUrl} variant="ghost" size="icon">
-                <Download />
-              </Button>
+            <Tooltip.Trigger class="hover:bg-gray-200/20">
+              {from.name || from.email}
             </Tooltip.Trigger>
             <Tooltip.Content
               class="bg-popover text-popover-foreground softborder"
             >
-              <p>Download as .eml</p>
+              <p>{from.email}</p>
             </Tooltip.Content>
           </Tooltip.Root>
-
-          <Button onclick={() => (open = !open)} variant="ghost" size="icon">
-            <span
-              class="transition pointer-events-none"
-              class:rotate-360={!open}
-              class:rotate-180={open}
+        {/each}
+        <span> -> </span>
+        {#each iter(email.to) as to}
+          <Tooltip.Root openDelay={100}>
+            <Tooltip.Trigger class="hover:bg-gray-200/20">
+              {to.name || to.email}
+            </Tooltip.Trigger>
+            <Tooltip.Content
+              class="bg-popover text-popover-foreground softborder"
             >
-              <ChevronDown />
-            </span>
-          </Button>
-        </span>
-      </div>
+              <p>{to.email}</p>
+            </Tooltip.Content>
+          </Tooltip.Root>
+        {/each}
+      </span>
     </div>
-    {#if email.html}
-      <iframe
-        class="collapsible w-full h-96 bg-white"
-        title={email.headers.Subject}
-        srcdoc={email.html}
-        class:open
-      >
-      </iframe>
-    {/if}
-  {:else}
-    <div>Loading...</div>
+    <div>
+      <span class="text-sm flex flex-col">
+        <Tooltip.Root openDelay={100}>
+          <Tooltip.Trigger>
+            <Button href={emailUrl} variant="ghost" size="icon">
+              <Download />
+            </Button>
+          </Tooltip.Trigger>
+          <Tooltip.Content
+            class="bg-popover text-popover-foreground softborder"
+          >
+            <p>Download as .eml</p>
+          </Tooltip.Content>
+        </Tooltip.Root>
+
+        <Button onclick={() => (open = !open)} variant="ghost" size="icon">
+          <span
+            class="transition pointer-events-none"
+            class:rotate-360={!open}
+            class:rotate-180={open}
+          >
+            <ChevronDown />
+          </span>
+        </Button>
+      </span>
+    </div>
+  </div>
+  {#if email.html}
+    <iframe
+      class="collapsible w-full h-96 bg-white"
+      title={email.headers.Subject}
+      srcdoc={email.html}
+      class:open
+    >
+    </iframe>
   {/if}
 </div>
 
