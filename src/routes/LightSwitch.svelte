b4f74f5ce54784aa5cf19188ca74d5b7e959e32a
Lyric <lyric@pc.dev>
2025-06-05 18:13:16 -0700

    enlarge theme button

--- src/routes/LightSwitch.svelte
+++ src/routes/LightSwitch.svelte
@@ -15,14 +15,14 @@
     class={cn(
       className,
       buttonVariants({ variant: "outline", size: "icon" }),
-      "fixed left-4 bottom-4 z-20"
+      "fixed left-4 bottom-4 z-20 w-12 h-12"
     )}
   >
     <SunIcon
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      class="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
     />
     <MoonIcon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      class="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
     />
     <span class="sr-only">Toggle theme</span>
   </DropdownMenu.Trigger>
