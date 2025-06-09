63f1a25266db1787f08e25b7eea103de84d02051
Lyric <lyric@pc.dev>
2025-06-08 23:01:14 -0700

    add discord button

--- src/routes/PageNav.svelte
+++ src/routes/PageNav.svelte
@@ -5,7 +5,11 @@
   import { ScrollArea } from "$lib/components/ui/scroll-area";
   import { searchRefs } from "./search";
   import { Button } from "$lib/components/ui/button";
-  import { Search } from "@lucide/svelte";
+  import {
+    Search,
+    MessagesSquare,
+    SquareArrowOutUpRight,
+  } from "@lucide/svelte";
 
   const { class: className = "" } = $props();
 
@@ -29,10 +33,23 @@
       searchRefs.openSearch();
     }}
     class="gap-2 text-muted-foreground hover:bg-muted focus:bg-muted
-  active:bg-muted border border-gray-600 justify-start mr-3 rounded-full"
+  active:bg-muted border border-gray-600 justify-start mr-3 rounded-full
+  cursor-pointer"
   >
     <Search size={16} />
     Search
+  </Button>
+  <Button
+    variant="secondary"
+    href="/discord"
+    onclick={() => {
+      window.dispatchEvent(new CustomEvent("resetNav"));
+    }}
+    class="gap-2 text-muted-foreground hover:text-foreground justify-start mr-3 rounded-md"
+  >
+    <!-- <MessagesSquare size="16" /> -->
+    <SquareArrowOutUpRight size={16} />
+    Discord
   </Button>
   <ScrollArea class="h-[calc(100vh-6rem)] pr-3">
     <div class="flex flex-col gap-2">
